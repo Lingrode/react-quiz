b7ec695fe2468cf7a14025a54d9029a5499f7f5f
Lingrode <grimaloilya@gmail.com>
2024-06-04 20:56:58 -0600

    quiz is ready

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./index.scss";
 
 const questions = [
@@ -26,40 +27,67 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({ correct }) {
   return (
     <div className="result">
       <img
         src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
         alt="congratulaion"
       />
-      <h2>You guessed 3 answers out of 10</h2>
-      <button>Try again</button>
+      <h2>
+        You guessed {correct} answers out of {questions.length}
+      </h2>
+      <a href="/">
+        <button>Try again</button>
+      </a>
     </div>
   );
 }
 
-function Game() {
+function Game({ question, onClickVariant, step }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
   return (
     <>
       <div className="progress">
-        <div style={{ width: "50%" }} className="progress__inner"></div>
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
       </div>
-      <h1>What is useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>This is a function to store component data</li>
-        <li>This is a global state</li>
-        <li>This is when no one needs you</li>
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={index}>
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
